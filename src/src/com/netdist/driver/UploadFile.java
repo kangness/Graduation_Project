d7daa2103a88034ad5d472ab1a6d6112bb4eda96
kangness <510391707@qq.com>
2013-03-19 11:18:06 +0800

    make file upload

--- src/src/com/netdist/driver/UploadFile.java
+++ src/src/com/netdist/driver/UploadFile.java
@@ -2,4 +2,14 @@ package com.netdist.driver;
 
 public class UploadFile {
 
+	private String Source;
+	private String Destination;
+	public UploadFile(){
+		
+	}
+	public UploadFile(String Source,String Destnation){
+		this.Source = Source;
+		this.Destination = Destination;
+	}
+	
 }
--- src/src/com/netdist/servlet/uploadfile.java
+++ src/src/com/netdist/servlet/uploadfile.java
@@ -1,11 +1,25 @@
 package com.netdist.servlet;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Iterator;
 import java.io.IOException;
+import java.util.List;
+
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.FileUploadBase.SizeLimitExceededException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 /**
  * Servlet implementation class uploadfile
@@ -27,6 +41,7 @@ public class uploadfile extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
@@ -34,6 +49,84 @@ public class uploadfile extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		final long MAX_SIZE = 30*1024*1024;
+		final String[] allowedExt = new String[]{
+				"jpg",
+				"jpeg",
+				"gif",
+				"txt",
+				"doc",
+				"mp3",
+				"wma",
+				"mp4",
+				"mkv"
+		};
+		response.setContentType("test/html");
+		response.setCharacterEncoding("UTF-8");
+		DiskFileItemFactory dfif = new DiskFileItemFactory();
+		dfif.setSizeThreshold(4096);
+		dfif.setRepository(new File (request.getRealPath("/")+"imagesUploadTemp"));
+		ServletFileUpload sfu = new ServletFileUpload(dfif);
+		sfu.setSizeMax(MAX_SIZE);
+		PrintWriter out = response.getWriter();
+		List  filelist = null;
+		try{
+			filelist = sfu.parseRequest(request);
+		}catch(FileUploadException ex){
+			out.println("upload file error");
+		}
+		if (filelist ==null || filelist.size() ==0){
+			out.println("could not find the file ");
+		}
+		Iterator fileItr = filelist.iterator();
+		while(fileItr.hasNext()){
+			FileItem fileItem = null;
+			String path = null;
+			long size  = 0;
+			fileItem = (FileItem)fileItr.next();
+			if(fileItem == null||fileItem.isFormField()){
+				continue;
+			}
+			path = fileItem.getName();
+			size = fileItem.getSize();
+			if("".equals(path)||size == 0){
+				out.println("Upload file error");
+				return ;
+			}
+			String t_name = path.substring(path.lastIndexOf("\\") + 1);
+			String t_ext = t_name.substring(t_name.lastIndexOf(".") + 1);
+			int allowFlag = 0;
+			int allowedExtCount = allowedExt.length;
+			for (; allowFlag < allowedExtCount; allowFlag++) {
+				if (allowedExt[allowFlag].equals(t_ext))
+					break;
+			}
+			if (allowFlag == allowedExtCount) {
+				out.println("请上传以下类型的文件\n\n");
+				for (allowFlag = 0; allowFlag < allowedExtCount; allowFlag++)
+					out.println("*." + allowedExt[allowFlag]+ "&nbsp;&nbsp;&nbsp;");
+			    return;
+			   }
+
+	
+			String u_name = request.getRealPath("/") +"imagesUploaded/"+t_name;
+			try{
+				fileItem.write(new File(u_name));
+				out.print("upload file  " + t_name + "  OK size "+size );
+			}
+			catch (Exception ex)
+			{
+				out.println("upload file "+t_name +" Error");
+			}
+		}
+		
+		
+		
+		
+		
+		}
+	public void destroy(){
+		super.destroy();
 	}
 
 }
